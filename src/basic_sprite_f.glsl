bce154d8d3cc7023f8c9c8c6a7b3835baba5819e
sir-irk <JeremyFMontgomery@gmail.com>
2016-01-15 19:18:29 -0500

    minor shader optimization and cleanup

--- src/basic_sprite_f.glsl
+++ src/basic_sprite_f.glsl
@@ -1,16 +1,10 @@
 #version 430 
 #extension GL_ARB_shader_storage_buffer_object : require
 
-//layout(std430, binding=1) buffer colorMods {
-//	vec4 colorMods[];
-//};
-
 in vec4 Color;
-in vec2 tex_coord;
 in flat int colorMod;
 out vec4 outColor;
-uniform sampler2D texA;
+
 void main() {
-    outColor = texture(texA, tex_coord) * (Color * colorMod);
-//    outColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    outColor = Color * colorMod;
 }
